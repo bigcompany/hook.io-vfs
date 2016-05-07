13ad7b85a2c99c0d03784241e87ec03af7d05558
Marak <marak.squires@gmail.com>
2016-05-07 07:41:20 -0400

    [view] Added RBAC and configurable adapters   * Adds Role Checks for all public API methods   * Role Check logic is configurable as fn   * Adds ability to pass in adapter config to API   * Updates UI and Documentation

--- view/api/v1/fs/layout.js
+++ view/api/v1/fs/layout.js
@@ -1,7 +1,9 @@
-var config = require('../../../../config');
 var vfs = require('../../../../');
 var psr = require('parse-service-request');
+var config = require('../../../../config');
+
 module['exports'] = function layout (opts, cb) {
+
   var $ = this.$,
   req = opts.req,
   res = opts.res;
@@ -23,12 +25,63 @@ module['exports'] = function layout (opts, cb) {
       return res.end('missing opts.unauthorizedRoleAccess - using default invalid access callback');
     }
 
-    checkRoleAccess({ req: opts.req, res: opts.res, role: "files::read" }, function (err, hasPermission) {
+
+    var mappings = {
+      "/writeFile": {
+        role: "files::writeFile"
+      },
+      "/removeFile": {
+        role: "files::removeFile"
+      },
+      "/readFile": {
+        role: "files::readFile"
+      },
+      "/readdir": {
+        role: "files::readdir"
+      },
+      "/stat": {
+        role: "files::stat"
+      },
+      "/createWriteStream": {
+        role: "files::createWriteStream"
+      },
+      "/createReadStream": {
+        role: "files::createReadStream"
+      }
+    };
+
+    var parsed = require('url').parse(req.url).pathname;
+
+    var _role = mappings[parsed] || "none";
+
+    if (_role === "none") {
+      return cb(null, $.html());
+    }
+    _role = _role.role;
+
+    checkRoleAccess({ req: opts.req, res: opts.res, role: _role }, function (err, hasPermission) {
+      //console.log("ROLE CHECK", err, hasPermission)
       if (!hasPermission) {
-        return res.end(unauthorizedRoleAccess(req, "files::read"));
+        return res.end(unauthorizedRoleAccess(req, _role));
       } else {
-        // console.log('using ad', params.adapter)
-        var client = vfs.createClient(config.adapters[params.adapter]);
+
+        //console.log('using config', params.adapter, config.adapters[params.adapter])
+        //console.log('owner', req.resource.owner)
+        // TODO: merge over with any sent in config options
+        var _config = {};
+        for(var p in config.adapters[params.adapter]) {
+          _config[p] = config.adapters[params.adapter][p];
+        }
+
+        if (req.resource.params && typeof req.resource.params.files === "object") {
+          for(var p in req.resource.params.files) {
+            _config[p] = req.resource.params.files[p];
+          }
+        }
+
+        _config.root = req.resource.owner;
+        // console.log('using API config', _config);
+        var client = vfs.createClient(_config);
         req.vfs = client;
         return cb(null, $.html());
       }
